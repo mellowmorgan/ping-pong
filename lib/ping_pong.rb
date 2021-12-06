c7a99748db6891b76bac3bde91a80a3c2c55865a
mellowmorgan <morgan.waites@gmail.com>
2021-12-06 09:46:47 -0800

    testing finished
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,10 +1,17 @@
+#!/usr/bin/ruby
 def ping_pong(x)
-  arr=[]
+  arr=[] 
   for a in 1..x do
-    if a%3==0
+    if a%3==0 && a%5==0
+      a="ping-pong"
+    elsif a%5==0
+      a="pong"
+    elsif a%3==0 
       a="ping"
     end
     arr.push(a)
   end
+  arr.each do |element|
+    puts element
   arr
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,7 +5,13 @@ describe('#ping_pong') do
   it("returns an array of ascending numbers up to the number entered") do
     expect(ping_pong(2)).to(eq([1,2]))
   end
-  it("prints each item in array of ascending number up to the number entered") do
+  it("replaces number divisible by 3 with ping") do
     expect(ping_pong(3)).to(eq([1,2,"ping"]))
+  end
+  it("replaces every mulitiple of 5 with pong") do
+    expect(ping_pong(5)).to(eq([1,2,"ping",4,"pong"]))
+  end
+  it("replaces number divisible by 3 and 5 with ping-pong") do
+    expect(ping_pong(15)).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping-pong"]))
   end
 end
